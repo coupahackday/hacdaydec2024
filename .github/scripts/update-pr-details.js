74e68bb71248e66c841677dba3c804d5ae80263e
coupahackday <coupahackdaydec@gmail.com>
2024-12-06 12:25:06 +0530

    Update update-pr-details.js 3

--- .github/scripts/update-pr-details.js
+++ .github/scripts/update-pr-details.js
@@ -40,6 +40,12 @@ const axios = require('axios');
     const prTitle = fields.summary || `Update related to ${ISSUE_KEY}`;
     // You can enrich PR body with more details from the Jira description and root cause if available.
     const prDescriptionParts = [
+      `## JIRA`
+        '',
+        `[MAIN_JIRA]:[${ISSUE_KEY}](${JIRA_BASE_URL}/browse/${ISSUE_KEY})`
+        '',
+      `* [Main JIRA ticket][MAIN_JIRA]`,
+      '',
       `**Jira Issue**: [${ISSUE_KEY}](${JIRA_BASE_URL}/browse/${ISSUE_KEY})`,
       '',
       `**Summary**: ${fields.summary}`,
